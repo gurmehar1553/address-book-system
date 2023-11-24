b4f08cd8c89c15b8fb08254f5d113ed2314b3b19
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-24 19:02:43 +0530

    Added: UC1 created contacts class for adding info

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -3,3 +3,19 @@ public class AddressBookSystem {
         System.out.println("Welcome to Address Book Program");
     }
 }
+
+
+class Contacts{
+        String fname, lname, city, state, email;
+        int zip, phone;
+        Contacts(String first,String last,String city_add,String state_add,String mail,int zip_add, int phoneNo){
+            fname = first;
+            lname = last;
+            city = city_add;
+            state = state_add;
+            email = mail;
+            zip = zip_add;
+            phone = phoneNo;
+        }
+}
+
